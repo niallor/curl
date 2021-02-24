cc4264e8477fff8f8492e60e80fdc902aedcad39
Niall <Niall.oReilly@ucd.ie>
2021-02-24 12:22:46 +0000

    Document CLI options --[no-]esni and --echconfig

---
+++ docs/cmdline-opts/ech.d
@@ -0,0 +1,14 @@
+Long: ech
+Help: Use encrypted Client Hello
+Protocols: TLS
+---
+Forces curl to attempt to use encrypted Client Hello or, as --no-ech,
+to avoid using encrypted Client Hello.
+
+Implied by use of the --echconfig option.
+
+If --ech and --no-ech are both specified, only the first one
+has effect.
+
+This description of the --ech option is PROVISIONAL, as
+ECH support is work in progress.
